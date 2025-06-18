07b467ed2b18350821b9f35eb9ffe91e9f8c7d91
gianlucca_pollo <gaguado@bancolombia.com.co>
2025-06-17 19:47:05 -0500

    Inputs
    changes: creacion formulario para agregar personajes.
    
        - eventos
        - referencias locales

--- Angular/01-bases/src/app/pages/dragonball-page/dragonball-page.component.ts
+++ Angular/01-bases/src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -1,6 +1,4 @@
 import {Component, computed, signal} from '@angular/core';
-import {RouterLink, RouterLinkActive} from '@angular/router';
-import {NgClass} from '@angular/common';
 
 interface Character {
   id: number;
@@ -15,8 +13,13 @@ interface Character {
   ]
 })
 export class DragonballPageComponent {
+
+  name = signal('gohan');
+  power = signal(10000);
+
+
   characters = signal<Character[]>([
-    {id: 1, name: 'Goku', power: 9001},
+    {id: 1, name: 'Goku', power: 9000},
     {id: 2, name: 'Vegeta', power: 8000},
     {id: 3, name: 'Piccolo', power: 7000},
     {id: 3, name: 'Yamcha', power: 100},
@@ -27,4 +30,29 @@ export class DragonballPageComponent {
   //     'text-danger': true,
   //   }
   // })
+  protected readonly parseInt = parseInt;
+
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() < 0) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    // this.characters().push(newCharacter);
+    this.characters.update(
+      list => [...list, newCharacter]
+    );
+
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
 }
