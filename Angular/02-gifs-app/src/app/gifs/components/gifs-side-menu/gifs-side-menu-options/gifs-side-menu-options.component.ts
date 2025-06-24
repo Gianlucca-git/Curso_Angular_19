56eedf0bb534f731a01ad897fd857fe3df6b8e5e
gaguado <gaguado@bancolombia.com.co>
2025-06-24 16:03:35 -0500

    RouterLinks Componentes Hijos
    changes:
       - uso de iconos
       - se muestran los componentes del menu iterativamente

--- Angular/02-gifs-app/src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
+++ Angular/02-gifs-app/src/app/gifs/components/gifs-side-menu/gifs-side-menu-options/gifs-side-menu-options.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+
+interface MenuOptions {
+  icon: string;
+  label: string;
+  router: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
   templateUrl: './gifs-side-menu-options.component.html',
+  imports: [
+    RouterLink, RouterLinkActive
+  ]
 })
 export class GifsSideMenuOptionsComponent {
+
+  menuOptions: MenuOptions[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      router: '/dashboard/trending',
+      subLabel: 'Gifs Populares',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Search',
+      router: '/dashboard/search',
+      subLabel: 'Buscador de Gifs',
+    },
+  ];
 
 }
