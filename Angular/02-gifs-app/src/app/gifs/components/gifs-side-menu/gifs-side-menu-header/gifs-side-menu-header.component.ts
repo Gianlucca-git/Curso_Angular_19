7993dc5802cbebbf438a24d6a5c442731808a546
gaguado <gaguado@bancolombia.com.co>
2025-06-24 16:32:11 -0500

    Environments & Path Alias
    changes:
       - generacion de environment por angular schematics
       - recorte de ruta de importacion de environments modificando el tsconfig.json
       - uso de variables de ambientes para el Header de la barra de menu

--- Angular/02-gifs-app/src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component.ts
+++ Angular/02-gifs-app/src/app/gifs/components/gifs-side-menu/gifs-side-menu-header/gifs-side-menu-header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {environment} from '@environments/environment';
+
+// import {environment} from '../../../../../environments/environment';
 
 @Component({
   selector: 'gifs-side-menu-header',
   templateUrl: './gifs-side-menu-header.component.html',
 })
 export class GifsSideMenuHeaderComponent {
+
+  envs = environment;
 
 }
---
+++ Angular/02-gifs-app/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companyNameSlogan: 'Maneja tus Gifs',
+  welcome: 'Bienvenido',
+
+};
