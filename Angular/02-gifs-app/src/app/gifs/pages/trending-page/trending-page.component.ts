f3fff864b961dee19f0264cb43723c5b40bcab00
gaguado <gaguado@bancolombia.com.co>
2025-06-24 18:18:30 -0500

    Input Imagenes
    changes:
       - Arreglo de imagenes que viaja entre 3 componentes usando Signals

--- Angular/02-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ Angular/02-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,10 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {GifsListComponent} from '../../components/gifs-list/gifs-list.component';
 
+
+const imagesUrls: string[] = [
+  'https://i.pinimg.com/736x/e7/44/fd/e744fd713c47fdcea1a324f4d12a532c.jpg',
+  'https://i.pinimg.com/736x/e7/44/fd/e744fd713c47fdcea1a324f4d12a532c.jpg',
+  'https://i.pinimg.com/736x/e7/44/fd/e744fd713c47fdcea1a324f4d12a532c.jpg',
+];
 @Component({
   selector: 'app-trending-page',
-  imports: [],
+  imports: [
+    GifsListComponent
+  ],
   templateUrl: './trending-page.component.html',
 })
 export default class TrendingPageComponent {
 
+  gifs = signal(imagesUrls);
 }
