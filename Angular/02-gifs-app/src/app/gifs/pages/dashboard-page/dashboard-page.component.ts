e1c6c03d36ed096b9f0f943c3211d3924966f3ca
gaguado <gaguado@bancolombia.com.co>
2025-06-24 15:32:06 -0500

    Menu lateral
    changes:
       - creacion de componente menu con componentes hijos header y options

--- Angular/02-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ Angular/02-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,12 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {GifsSideMenuComponent} from '../../components/gifs-side-menu/gifs-side-menu.component';
 
 @Component({
   selector: 'app-dashboard-page',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    GifsSideMenuComponent,
   ],
   templateUrl: './dashboard-page.component.html',
 })
