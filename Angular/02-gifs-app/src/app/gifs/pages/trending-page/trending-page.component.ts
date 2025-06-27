77149e79f0d7bb1affc5e9279c919a731e93010a
gaguado <gaguado@bancolombia.com.co>
2025-06-27 16:43:09 -0500

    viewChild - referencias del template
    changes:
       -

--- Angular/02-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ Angular/02-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,18 +1,19 @@
-import {Component, inject, signal} from '@angular/core';
-import {GifsListComponent} from '../../components/gifs-list/gifs-list.component';
+import {Component, ElementRef, inject, signal, viewChild} from '@angular/core';
 import {GifsService} from '../../services/gifs.service';
 
-// const imagesUrls: string[] = [
-//   'https://i.pinimg.com/736x/e7/44/fd/e744fd713c47fdcea1a324f4d12a532c.jpg',
-// ];
 
 @Component({
   selector: 'app-trending-page',
-  imports: [
-    GifsListComponent
-  ],
+  imports: [],
   templateUrl: './trending-page.component.html',
 })
 export default class TrendingPageComponent {
   gifService = inject(GifsService);
+  protected readonly onscroll = onscroll;
+
+
+  scrollDivRef = viewChild<ElementRef>('groupDiv');
+  onScroll($event: Event) {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+  }
 }
