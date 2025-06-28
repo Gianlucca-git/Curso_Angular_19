62bffd82625e3b76c9fa7e7bcefa95150a4d4c6a
gaguado <gaguado@bancolombia.com.co>
2025-06-28 18:46:11 -0500

    Mantener Scroll
    changes:
       - servicio para guardar el valor del scroll TOP

--- Angular/02-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ Angular/02-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,5 +1,6 @@
-import {Component, ElementRef, inject, signal, viewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, inject, signal, viewChild} from '@angular/core';
 import {GifsService} from '../../services/gifs.service';
+import {ScrollStateService} from '../../../shared/service/scroll-state.service';
 
 
 @Component({
@@ -7,11 +8,17 @@ import {GifsService} from '../../services/gifs.service';
   imports: [],
   templateUrl: './trending-page.component.html',
 })
-export default class TrendingPageComponent {
+export default class TrendingPageComponent implements AfterViewInit {
   gifService = inject(GifsService);
+  scrollStateService = inject(ScrollStateService);
   protected readonly onscroll = onscroll;
 
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
 
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+  }
   scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
 
   onScroll(event: Event) {
@@ -21,8 +28,9 @@ export default class TrendingPageComponent {
     const scrollTop = scrollDiv.scrollTop;
     const clientHeight = scrollDiv.clientHeight;
     const scrollHeight = scrollDiv.scrollHeight;
-
     const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
+
+    this.scrollStateService.trendingScrollState.set(scrollTop);
 
     if (isAtBottom) {
       this.gifService.loadTrendingGifs();
