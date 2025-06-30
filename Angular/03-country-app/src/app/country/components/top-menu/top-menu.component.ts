93c279d95569f1b8976db04705a5ab38771fdce3
gaguado <gaguado@bancolombia.com.co>
2025-06-30 12:41:18 -0500

    Menu Superior
    changes:
       - uso de Iconify para traer iconos

---
+++ Angular/03-country-app/src/app/country/components/top-menu/top-menu.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+
+@Component({
+  selector: 'county-top-menu',
+  imports: [
+    RouterLink, RouterLinkActive
+  ],
+  templateUrl: './top-menu.component.html',
+})
+export class TopMenuComponent {
+
+}
