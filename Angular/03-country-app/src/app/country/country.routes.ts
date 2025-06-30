2df853661b1365f1b4a97b69bd558fa7da986f64
gaguado <gaguado@bancolombia.com.co>
2025-06-30 11:56:42 -0500

    layout component
    changes:
       -

--- Angular/03-country-app/src/app/country/country.routes.ts
+++ Angular/03-country-app/src/app/country/country.routes.ts
@@ -1,14 +1,23 @@
 import {Routes} from '@angular/router';
 import {ByCapitalPageComponent} from './pages/by-capital-page/by-capital-page.component';
+import {CountryLayoutComponent} from './layouts/country-layout/country-layout.component';
 
 export const countryRoutes: Routes = [
   {
     path: '',
-    component: ByCapitalPageComponent,
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'capital',
+        component: ByCapitalPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'capital',
+      },
+    ]
   },
-  // {
-  //   path: '',
-  // },
+
 
 ];
 
