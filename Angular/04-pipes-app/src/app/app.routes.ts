38c4028c538f11a0158cee5b2fd628fb003db06f
gaguado <gaguado@bancolombia.com.co>
2025-07-03 14:53:44 -0500

    Componentes Principales
    changes:
        - rutas
        - componentes pages principales

--- Angular/04-pipes-app/src/app/app.routes.ts
+++ Angular/04-pipes-app/src/app/app.routes.ts
@@ -1,3 +1,32 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basic',
+    title: 'Pipes Basics',
+    loadComponent: () =>
+      import('./pages/basic-page/basic-page.component'),
+  },
+  {
+    path: 'numbers',
+    title: 'Pipes Numbers',
+    loadComponent: () =>
+      import('./pages/number-page/number-page.component'),
+  },
+  {
+    path: 'uncommon',
+    title: 'Pipes UnCommons',
+    loadComponent: () =>
+      import('./pages/uncommon-page/uncommon-page.component'),
+  },
+  {
+    path: 'custom',
+    title: 'Pipes Personal',
+    loadComponent: () =>
+      import('./pages/custom-page/custom-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'basic',
+  },
+];
---
+++ Angular/04-pipes-app/src/app/pages/number-page/number-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-number-page',
+  imports: [],
+  templateUrl: './number-page.component.html',
+})
+export default class NumberPageComponent {
+
+}
