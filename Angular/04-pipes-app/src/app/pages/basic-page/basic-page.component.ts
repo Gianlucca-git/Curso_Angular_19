9053424796436303ba25d11b9179e60d7c52c7e7
gaguado <gaguado@bancolombia.com.co>
2025-07-04 14:58:01 -0500

    Pipes: lower upper title
    changes:
        -

--- Angular/04-pipes-app/src/app/pages/basic-page/basic-page.component.ts
+++ Angular/04-pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {LowerCasePipe, TitleCasePipe, UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [
+    LowerCasePipe,
+    UpperCasePipe,
+    TitleCasePipe
+  ],
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
+
+  nameLower = signal('gianlucca');
+  nameUpper = signal('GIANLUCCA');
+  fullName = signal('GianLuccA');
 
 }
