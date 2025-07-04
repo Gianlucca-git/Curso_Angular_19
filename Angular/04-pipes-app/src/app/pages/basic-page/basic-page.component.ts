44c6e409bcb6e988919e0c4c7bf687a0eca63056
gaguado <gaguado@bancolombia.com.co>
2025-07-04 15:23:26 -0500

    Pipes Date
    changes:
        - effecto para refrescar la senial del nowDate

--- Angular/04-pipes-app/src/app/pages/basic-page/basic-page.component.ts
+++ Angular/04-pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,13 @@
-import {Component, signal} from '@angular/core';
-import {LowerCasePipe, TitleCasePipe, UpperCasePipe} from '@angular/common';
+import {Component, effect, signal} from '@angular/core';
+import {DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-basic-page',
   imports: [
     LowerCasePipe,
     UpperCasePipe,
-    TitleCasePipe
+    TitleCasePipe,
+    DatePipe
   ],
   templateUrl: './basic-page.component.html',
 })
@@ -16,4 +17,13 @@ export default class BasicPageComponent {
   nameUpper = signal('GIANLUCCA');
   fullName = signal('GianLuccA');
 
+  nowDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.nowDate.set(new Date());
+      console.log('nowDate');
+    }, 1000)
+    onCleanup(() => clearInterval(interval));
+  })
 }
