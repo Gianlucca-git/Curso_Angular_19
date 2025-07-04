094d9e5f51862967e7d23afe6b6a2075bb394e0a
gaguado <gaguado@bancolombia.com.co>
2025-07-04 15:08:05 -0500

    Pipes Numericos
    changes:
        -

--- Angular/04-pipes-app/src/app/pages/number-page/number-page.component.ts
+++ Angular/04-pipes-app/src/app/pages/number-page/number-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {CurrencyPipe, DecimalPipe, LowerCasePipe, PercentPipe, TitleCasePipe, UpperCasePipe} from "@angular/common";
 
 @Component({
   selector: 'app-number-page',
-  imports: [],
+  imports: [
+    LowerCasePipe,
+    TitleCasePipe,
+    UpperCasePipe,
+    DecimalPipe,
+    PercentPipe,
+    CurrencyPipe
+  ],
   templateUrl: './number-page.component.html',
 })
 export default class NumberPageComponent {
 
+  totalSells = signal(2_433_323.1234);
+  percent = signal(0.46);
 }
