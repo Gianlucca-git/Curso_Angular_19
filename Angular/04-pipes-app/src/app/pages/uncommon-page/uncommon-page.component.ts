8f367a4e265f4933716454aacacbdc2f92982388
gaguado <gaguado@bancolombia.com.co>
2025-07-07 10:21:58 -0500

    i18nSelectPipe
    changes:
        - pipe de internacionalizacion

--- Angular/04-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ Angular/04-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,17 +1,46 @@
-import {Component} from '@angular/core';
-import {LowerCasePipe, TitleCasePipe, UpperCasePipe} from "@angular/common";
+import {Component, signal} from '@angular/core';
 import {StatComponent} from '../../components/stat/stat.component';
+import {I18nSelectPipe} from '@angular/common';
+
+const client1 = {
+  name: 'Pepita',
+  gender: 'female',
+  age: 40,
+  address: 'Otawa, Canada',
+};
+
+const client2 = {
+  name: 'Gian',
+  gender: 'male',
+  age: 40,
+  address: 'Valle, COL',
+};
 
 @Component({
   selector: 'app-uncommon-page',
   imports: [
-    LowerCasePipe,
-    TitleCasePipe,
-    UpperCasePipe,
-    StatComponent
+    StatComponent, I18nSelectPipe
   ],
   templateUrl: './uncommon-page.component.html',
 })
 export default class UncommonPageComponent {
+
+  // i18n Select
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  }
+
+  changeClient() {
+    if (this.client() == client1) {
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+
+  }
 
 }
