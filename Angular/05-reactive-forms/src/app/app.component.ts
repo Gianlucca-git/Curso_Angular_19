f05f0335c44fb804ac63b760f112ba92256e6cb1
gaguado <gaguado@bancolombia.com.co>
2025-07-08 10:22:48 -0500

    Inicio estructura proyecto
    changes:
        - Creacion de Rutas
        - componentes para formularios
        - componentes compartidos
        - utils
        - componente autenticacion

--- Angular/05-reactive-forms/src/app/app.component.ts
+++ Angular/05-reactive-forms/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SideMenuComponent} from './share/components/side-menu/side-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenuComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
---
+++ Angular/05-reactive-forms/src/app/auth/auth.routes.ts
@@ -0,0 +1,18 @@
+import {RegisterPageComponent} from './pages/register-page/register-page.component';
+import {Routes} from '@angular/router';
+
+export const authRoutes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'sing-up',
+        component: RegisterPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'sing-up',
+      }
+    ],
+  },
+]
