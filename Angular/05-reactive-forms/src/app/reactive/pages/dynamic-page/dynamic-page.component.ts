e48e0df50e8d7db3be0115db73bae29d7f5c3afb
gaguado <gaguado@bancolombia.com.co>
2025-07-09 23:33:59 -0500

    [Dynamic-Form] Arrays en Forms
    changes:
        - this.formBuilder.array
        - get favoriteGames()

--- Angular/05-reactive-forms/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ Angular/05-reactive-forms/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {JsonPipe} from '@angular/common';
 import {HeaderComponentComponent} from '../../header-component/header-component.component';
+import {FormUtils} from '../../../utils/form.utils';
+import {FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators as v} from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
   imports: [
     JsonPipe,
-    HeaderComponentComponent
+    HeaderComponentComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './dynamic-page.component.html',
 })
 export class DynamicPageComponent {
 
+  private formUtils = FormUtils;
+
+  private formBuilder = inject(FormBuilder);
+  myForm: FormGroup = this.formBuilder.group({
+    name: ['',
+      [v.required, v.minLength(3)]
+    ],
+    favoriteGames: this.formBuilder.array([
+      ['Metal Gear', v.required],
+      ['Super Mario', v.required],
+    ], v.minLength(3)),
+  });
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
 }
